83e3f39ab0d4a288b62db57e9650900a00c2cfb6
agconti <agc11d@gmail.com>
2015-11-06 19:19:38 -0500

    feat(): added game logic, count and win conditions

--- rollerball/Assets/Scripts/PlayerController.cs
+++ rollerball/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
+	public Text countText;
+	public Text winText;
+
 	private Rigidbody rb;
+	private int count;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text =  "";
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+
+		if (count >= 12) {
+			winText.text = "You win!!";
+		}
 	}
 
 	void FixedUpdate () {
@@ -22,6 +38,8 @@ public class PlayerController : MonoBehaviour {
 
 		if (other.CompareTag("Pick Up")) {
 			other.gameObject.SetActive(false);
+			count++;
+			SetCountText ();
 		}
 	}
 }
